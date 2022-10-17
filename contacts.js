1051f863d4e32c19b1e8a7745f89e53762fcc8ec
Halyna Melnykova <galyamelnykova@gmail.com>
2022-10-17 22:03:21 +0300

    add functions

--- contacts.js
+++ contacts.js
@@ -1,23 +1,65 @@
-const fs = require("fs").promises;
+// const fs = require("fs").promises;
+const fs = require("fs/promises");
+const path = require("path");
+const { nanoid } = require("nanoid");
 
-/*
- * Розкоментуйте і запиши значення
- * const contactsPath = ;
- */
+const contactsPath = path.join(__dirname, "db/contacts.json");
 
-// TODO: задокументувати кожну функцію
-function listContacts() {
-  // ...твій код
-}
+const updateContacts = async (contacts) =>
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
 
-function getContactById(contactId) {
-  // ...твій код
-}
+// TODO: задокументувати кожну функцію ???
 
-function removeContact(contactId) {
-  // ...твій код
-}
+const listContacts = async () => {
+  const data = await fs.readFile(contactsPath);
+  return JSON.parse(data);
+};
 
-function addContact(name, email, phone) {
-  // ...твій код
-}
+const getContactById = async (contactId) => {
+  const contacts = await listContacts();
+  const result = contacts.find((item) => item.id === contactId);
+  return result || null;
+};
+
+const removeContact = async (contactId) => {
+  const contacts = await listContacts();
+  const index = contacts.findIndex((item) => item.id === contactId);
+  if (index === -1) {
+    return null;
+  }
+  const [result] = contacts.splice(index, 1);
+  await updateContacts(contacts);
+  return result;
+};
+
+const addContact = async ({ name, email, phone }) => {
+  const contacts = await listContacts();
+  const newContact = {
+    id: nanoid(),
+    name,
+    email,
+    phone,
+  };
+  contacts.push(newContact);
+  await updateContacts(contacts);
+  return newContact;
+};
+
+const updateContactById = async (contactId, data) => {
+  const contacts = await listContacts();
+  const index = contacts.findIndex((item) => item.id === contactId);
+  if (index === -1) {
+    return null;
+  }
+  contacts[index] = { contactId, ...data };
+  await updateContacts(contacts);
+  return contacts[index];
+};
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+  updateContactById,
+};
